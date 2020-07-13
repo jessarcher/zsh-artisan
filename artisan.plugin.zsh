3918bbdaa9d88e785bbb59ce78c347515fa91d6f
Jess Archer <jess@jessarcher.com>
2020-07-13 15:05:14 +1000

    Use local variables

--- artisan.plugin.zsh
+++ artisan.plugin.zsh
@@ -7,55 +7,55 @@
 # adds shell completions that work anywhere artisan can be located.
 
 function artisan() {
-    _artisan=`_artisan_find`
+    local artisan_path=`_artisan_find`
 
-    if [ "$_artisan" = "" ]; then
+    if [ "$artisan_path" = "" ]; then
         >&2 echo "zsh-artisan: You seem to have upset the delicate internal balance of my housekeeper."
         return 1
     fi
 
-    _artisan_laravel_path=`dirname $_artisan`
-    _artisan_docker_compose=`find $_artisan_laravel_path -maxdepth 1 -regex ".*/docker-compose.ya?ml" | head -n1`
-    _artisan_docker_compose_service=''
+    local laravel_path=`dirname $artisan_path`
+    local docker_compose_config_path=`find $laravel_path -maxdepth 1 -regex ".*/docker-compose.ya?ml" | head -n1`
+    local docker_compose_service_name=''
 
-    if [ "$_artisan_docker_compose" != '' ]; then
-        _artisan_docker_compose_service=`docker-compose ps --services 2>/dev/null | grep 'app\|php\|api\|workspace' | head -n1`
+    if [ "$docker_compose_config_path" != '' ]; then
+        docker_compose_service_name=`docker-compose ps --services 2>/dev/null | grep 'app\|php\|api\|workspace' | head -n1`
     fi
 
-    _artisan_start_time=`date +%s`
+    local artisan_start_time=`date +%s`
 
-    if [ "$_artisan_docker_compose_service" != '' ]; then
+    if [ "$docker_compose_service_name" != '' ]; then
         if [ -t 1 ]; then
-            docker-compose exec $_artisan_docker_compose_service php artisan $*
+            docker-compose exec $docker_compose_service_name php artisan $*
         else
             # The command is not being run in a TTY (e.g. it's being called by the completion handler below)
-            docker-compose exec -T $_artisan_docker_compose_service php artisan $*
+            docker-compose exec -T $docker_compose_service_name php artisan $*
         fi
     else
-        php $_artisan $*
+        php $artisan_path $*
     fi
 
-    _artisan_exit_status=$? # Store the exit status so we can return it later
+    local artisan_exit_status=$? # Store the exit status so we can return it later
 
     if [[ $1 = "make:"* && $ARTISAN_OPEN_ON_MAKE_EDITOR != "" ]]; then
         # Find and open files created by artisan
         find \
-            "$_artisan_laravel_path/app" \
-            "$_artisan_laravel_path/tests" \
-            "$_artisan_laravel_path/database" \
+            "$laravel_path/app" \
+            "$laravel_path/tests" \
+            "$laravel_path/database" \
             -type f \
-            -newermt "-$((`date +%s` - $_artisan_start_time + 1)) seconds" \
+            -newermt "-$((`date +%s` - $artisan_start_time + 1)) seconds" \
             -exec $ARTISAN_OPEN_ON_MAKE_EDITOR {} \; 2>/dev/null
     fi
 
-    return $_artisan_exit_status
+    return $artisan_exit_status
 }
 
 compdef _artisan_add_completion artisan
 
 function _artisan_find() {
     # Look for artisan up the file tree until the root directory
-    dir=.
+    local dir=.
     until [ $dir -ef / ]; do
         if [ -f "$dir/artisan" ]; then
             echo "$dir/artisan"
