f450ca12ed5377a3c86b8cdfeee97e362b071c74
Jess Archer <jess@jessarcher.com>
2022-05-22 14:31:04 +1000

    Prefer sail, then docker compose, then docker-compose

--- artisan.plugin.zsh
+++ artisan.plugin.zsh
@@ -16,24 +16,28 @@ function artisan() {
 
     local laravel_path=`dirname $artisan_path`
     local docker_compose_config_path=`find $laravel_path -maxdepth 1 -regex ".*/docker-compose.ya?ml" | head -n1`
-    local docker_compose_service_name=''
+    local artisan_cmd
 
-    if [ "$docker_compose_config_path" != '' ]; then
-        docker_compose_service_name=`docker-compose ps --services 2>/dev/null | grep 'app\|php\|api\|workspace\|laravel\.test\|webhost' | head -n1`
+    if [ "$docker_compose_config_path" = '' ]; then
+        artisan_cmd="php $artisan_path"
+    else
+        if [ "`grep "laravel/sail" $docker_compose_config_path | head -n1`" != '' ]; then
+            artisan_cmd="$laravel_path/vendor/bin/sail artisan"
+        else
+            local docker_compose_cmd=`_docker_compose_cmd`
+            local docker_compose_service_name=`$docker_compose_cmd ps --services 2>/dev/null | grep 'app\|php\|api\|workspace\|laravel\.test\|webhost' | head -n1`
+            if [ -t 1 ]; then
+                artisan_cmd="$docker_compose_cmd exec $docker_compose_service_name php artisan"
+            else
+                # The command is not being run in a TTY (e.g. it's being called by the completion handler below)
+                artisan_cmd="$docker_compose_cmd exec -T $docker_compose_service_name php artisan"
+            fi
+        fi
     fi
 
     local artisan_start_time=`date +%s`
 
-    if [ "$docker_compose_service_name" != '' ]; then
-        if [ -t 1 ]; then
-            docker-compose exec $docker_compose_service_name php artisan $*
-        else
-            # The command is not being run in a TTY (e.g. it's being called by the completion handler below)
-            docker-compose exec -T $docker_compose_service_name php artisan $*
-        fi
-    else
-        php $artisan_path $*
-    fi
+    eval $artisan_cmd $*
 
     local artisan_exit_status=$? # Store the exit status so we can return it later
 
@@ -76,4 +80,13 @@ function _artisan_add_completion() {
 
 function _artisan_get_command_list() {
     artisan --raw --no-ansi list | sed "s/[[:space:]].*//g"
+}
+
+function _docker_compose_cmd() {
+    docker compose &> /dev/null
+    if [ $? = 0 ]; then
+        echo "docker compose"
+    else
+        echo "docker-compose"
+    fi
 }
